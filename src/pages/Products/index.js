60be22b1ccc0b47a2a073fc505b3c524147392d4
lucas paiva <lukspaiva95@gmail.com>
2021-10-22 19:37:57 -0300

    Use search term as page title

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Page } from "./../../layout";
 import { PageLoader, ProductList } from "./../../components";
-import { selectProductsLoading } from "./../../redux/ducks/Product/selectors";
+import {
+  selectProductsLoading,
+  selectProductPayload,
+} from "./../../redux/ducks/Product/selectors";
 import { Container } from "./styles";
 
 const ProductsPage = () => {
+  const [title, setTitle] = useState("Produtos");
+
   const loading = useSelector(selectProductsLoading);
+  const payload = useSelector(selectProductPayload);
+
+  useEffect(() => {
+    setTitle(payload && payload.search ? payload.search : "Produtos");
+  }, [payload]);
 
   return (
-    <Page title="Produtos">
+    <Page title={title}>
       <PageLoader loading={loading} />
       <Container>
         <ProductList />
