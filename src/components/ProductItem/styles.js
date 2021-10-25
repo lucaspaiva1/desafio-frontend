e6b68dd2873735fdf621f2dfa9fa8ff16cb17ecd
lucas paiva <lukspaiva95@gmail.com>
2021-10-25 12:03:16 -0300

    improve product image layout

--- src/components/ProductItem/styles.js
+++ src/components/ProductItem/styles.js
@@ -15,7 +15,7 @@ export const ImageContainer = styled.div`
 export const ProductImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
 `;
 
 export const DescriptionContainer = styled.div`
